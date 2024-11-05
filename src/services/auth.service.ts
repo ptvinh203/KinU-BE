8da66df91223cd997c056cd25ca110be27a7759f
tmquan5823 <tmquan0948628477@gmail.com>
2024-11-05 23:06:19 +0700

    Login fix

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,12 +4,17 @@ import { Account } from '../models/Account'
 import { AppDataSource } from '../config/data-source'
 import NotFoundError from '@src/errors/NotFoundError'
 import UnauthorizedError from '@src/errors/UnauthorizedError'
+import BadRequestError from '@src/errors/BadRequestError'
 
 const login = async (req: Request) => {
   try {
     const { username, password } = req.body
     const userRepo = AppDataSource.getRepository(Account)
+    if(!username || !password){
+      throw new BadRequestError("Tham số username và password không hợp lệ!")
+    }
     const existingAccount = await userRepo.findOne({ where: { username } })
+
     if (!existingAccount) {
       throw new NotFoundError('Tài khoản không hợp lệ!')
     }
